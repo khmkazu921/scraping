230fe65915a76a4a2faaaa41da6e3ccbbacc58a3
Kazuki Furukawa <13583600+khmkazu921@users.noreply.github.com>
2020-04-26 15:32:46 +0900

    remove unneccessary files

--- update_table.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-if /usr/bin/php -d display_errors=1 -d error_reporting=2147483647 script/insert.php  && /usr/bin/php -d display_errors=1 -d error_reporting=2147483647 script/diff.php; then
-   echo "\e[1m\e[33mUpdate Success !";
-else
-   echo "\e[1m\e[33mUpdate Failed !";
-fi
- 
