2e61129d0f801f3fc58cdd7778dd599edcd29914
Kazuki Furukawa <13583600+khmkazu921@users.noreply.github.com>
2020-09-23 10:24:26 +0900

    update update.php

--- script/update.php
+++ script/update.php
@@ -1,54 +1,53 @@
 <?php
-echo "<h1>情報を更新しています...</h1>";
-echo str_pad(" ",4096)."<br/>";
+header("Content-Type: text/html; charset=UTF-8");
+set_time_limit( 0 );
+echo str_repeat( ' ', 1024 );
 
+echo "<h1>情報を更新しています...</h1>";
 ob_start();
 ob_end_flush();
 ob_start('mb_output_handler');
-
-set_time_limit( 0 );
-
 ini_set('display_errors', 1);
 ini_set('error_reporting', E_ALL);
 
 // get json from remote
+set_include_path(get_include_path().':/home/k-consulting-o/www/wp2/regulation/script/phpseclib');
 
-set_include_path(get_include_path().':/home/k-consulting-o/www/scraping/script/phpseclib');
-echo get_include_path();
-
+//echo get_include_path();
 include('Net/SSH2.php');
 include('Crypt/RSA.php');
 
 echo " Connecting SSH...<br/>";
-
-MyFlush();
+ob_flush();
+flush();
 $key = new Crypt_RSA();
 $file = file_get_contents('/home/k-consulting-o/.ssh/scraping_rsa');
 if(is_null($file)) {
     echo 'Failed';
 }
 $key->loadKey($file);
-$ssh = new Net_SSH2('153.127.44.91');
+$ssh = new Net_SSH2('***.***.***.***');
 
 try {
     if (!$ssh->login('scraping', $key)) {
         throw new Exception('SSH Autentication rejected by server<br/>');
     }
     echo "SSH Login Success<br/>";
-    MyFlush();
-
-    echo "Exec /usr/bin/python3 /opt/scraping/test.py...<br/>";
     ob_flush();
     flush();
 
-    echo $ssh->exec('/usr/bin/python3 /opt/scraping/test.py');
-
-    echo "Finish Exec /usr/bin/python3 /opt/scraping/test.py<br/>";
-
+    echo "Exec python3 /home/scraping/test.py now <br/>";
+    echo "Please wait...<br/>";
     ob_flush();
     flush();
 
-    $string = $ssh->exec('cat /opt/scraping/data');
+    echo $ssh->exec('/usr/bin/python3 /home/scraping/test.py');
+
+    echo "Finish Exec python3 /home/scraping/test.py<br/>";
+    ob_flush();
+    flush();
+
+    $string = $ssh->exec('cat /home/scraping/data');
 
     echo " Finish Exec cat /opt/scraping/data<br/>";
     ob_flush();
@@ -63,6 +62,7 @@ try {
     flush();
 
     $getdata = json_decode($string);
+    // var_dump($getdata);
     if (empty($getdata)) {
 	throw new Exception('Decode JSON Failed');
     }
@@ -82,8 +82,8 @@ ob_flush();
 flush();
 
 // connect database
-$dsn = 'mysql:dbname=scraping;host=localhost';
-$u='user'; $pw='pass';
+$dsn = 'mysql:dbname=**********;host=************';
+$u='***********'; $pw='**********';
 $pdo = new PDO($dsn, $u, $pw);
 
 // truncate table
@@ -101,6 +101,7 @@ echo "Finish Truncate<br/>";
 ob_flush();
 flush();
 
+
 $array = $getdata;
 
 // insert data into mysql database
@@ -114,6 +115,7 @@ foreach ($array as &$a) {
 	    continue;
 	}
 	$b = array($a->title,$a->id,$a->updated,$a->site,$a->link);
+    	// var_dump($b);
 	$pdo->beginTransaction();
 	$pdo->prepare($sql)->execute($b);
 	$pdo->commit();
@@ -126,6 +128,23 @@ foreach ($array as &$a) {
 }
 
 echo "Finish Inserting</br>";
+ob_flush();
+flush();
+
+$delete_old = "DELETE FROM `olddata` WHERE (`updated` < DATE_SUB(CURDATE(), INTERVAL 1 YEAR))";
+$delete_new = "DELETE FROM `newdata` WHERE (`updated` < DATE_SUB(CURDATE(), INTERVAL 1 YEAR))";
+$stmt1 = $pdo->prepare($delete_old);
+$stmt2 = $pdo->prepare($delete_new);
+try {
+    $stmt1->execute();
+    $stmt2->execute();
+} catch (Exception $e){
+    echo $e->getMessage();
+    ob_flush();
+    flush();
+}
+
+echo "Finish Delete Olddata<br/>";
 ob_flush();
 flush();
 
